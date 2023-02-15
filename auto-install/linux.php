fd2d44078459207dd7385b4a520ad9010c892422
Dichvucoder <119738964+Dichvucoder@users.noreply.github.com>
2023-02-15 20:39:21 +0700

    Update linux.php

--- auto-install/linux.php
+++ auto-install/linux.php
@@ -1,9 +1,9 @@
 print_r("INSTALL TUR-REPO ...\n");
-system("pkg i tur-repo -y");
+system("pkg i tur-repo -y >install.log");
 print_r("INSTALL DICHVUCODER-REPO ...\n");
-system("pkg i dichvucoder-repo -y");
+system("pkg i dichvucoder-repo -y >install.log");
 print_r("INSTALL PHP 7 ...\n");
-system("pkg i php7 -y");
+system("pkg i php7 -y >install.log");
 print_r("INSTALL DICHVUCODER EXTENSION");
-system("pkg i php7-dichvucoder -y");
+system("pkg i php7-dichvucoder -y >install.log");
 print_r("AUTO INSTALL DONE\n");
